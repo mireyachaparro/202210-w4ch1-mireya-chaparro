ef4cab7ccf874f4ee8f3e5f35ea1549714a2b663
mireyachaparro <mireyacn99@gmail.com>
2022-10-24 19:37:23 +0200

    Add item component

--- src/components/items.tsx
+++ src/components/items.tsx
@@ -1,31 +1,39 @@
+import { info } from "../models/data";
+
 export function Items() {
   return (
-    <li className="gentleman">
-      <div className="gentleman__avatar-container">
-        <img
-          className="gentleman__avatar"
-          src="../assets/fary.jpg"
-          alt="The Fary pointing at you"
-        />
-        <span className="gentleman__initial">F</span>
-      </div>
-      <div className="gentleman__data-container">
-        <h2 className="gentleman__name">The Fary</h2>
-        <ul className="gentleman__data-list">
-          <li className="gentleman__data">
-            <span className="gentleman__data-label">Profession:</span>
-            Influencer
-          </li>
-          <li className="gentleman__data">
-            <span className="gentleman__data-label">Status:</span> RIP
-          </li>
-          <li className="gentleman__data">
-            <span className="gentleman__data-label">Twitter:</span> Pending
-          </li>
-        </ul>
-      </div>
-      <i className="icon gentleman__icon fas fa-check"></i>
-      <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
-    </li>
+    <>
+      {info.map((item) => (
+        <li className="gentleman">
+          <div className="gentleman__avatar-container">
+            <img
+              className="gentleman__avatar"
+              src={"../assets/" + item.picture}
+              alt={item.alternativeText}
+            />
+            <span className="gentleman__initial">F</span>
+          </div>
+          <div className="gentleman__data-container">
+            <h2 className="gentleman__name">{item.name}</h2>
+            <ul className="gentleman__data-list">
+              <li className="gentleman__data">
+                <span className="gentleman__data-label">Profession:</span>
+                {item.profession}
+              </li>
+              <li className="gentleman__data">
+                <span className="gentleman__data-label">Status:</span>
+                {item.status}
+              </li>
+              <li className="gentleman__data">
+                <span className="gentleman__data-label">Twitter:</span>
+                {item.twitter}
+              </li>
+            </ul>
+          </div>
+          <i className="icon gentleman__icon fas fa-check"></i>
+          <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+        </li>
+      ))}
+    </>
   );
 }
